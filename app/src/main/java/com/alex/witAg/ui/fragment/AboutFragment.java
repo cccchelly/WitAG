9b11d500a21791010afb28baa2b4ea0191e3aa73
liuxiangdong <liuxiangdong@chinazhix.com>
2018-06-22 18:00:39 +0800

    20180622域名添加

--- app/src/main/java/com/alex/witAg/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/alex/witAg/ui/fragment/AboutFragment.java
@@ -2,14 +2,22 @@ package com.alex.witAg.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.alex.witAg.R;
 import com.alex.witAg.base.BaseFragment;
 import com.alex.witAg.presenter.AboutPresenter;
 import com.alex.witAg.presenter.viewImpl.IAboutView;
+import com.alex.witAg.utils.AppMsgUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by dth
@@ -17,7 +25,11 @@ import com.alex.witAg.presenter.viewImpl.IAboutView;
  * Date: 2018-03-08.
  */
 
-public class AboutFragment extends BaseFragment<AboutPresenter,IAboutView> implements IAboutView {
+public class AboutFragment extends BaseFragment<AboutPresenter, IAboutView> implements IAboutView {
+    @BindView(R.id.about_imei)
+    TextView mImei;
+    Unbinder unbinder;
+
     @Override
     protected void fetchData() {
 
@@ -25,7 +37,7 @@ public class AboutFragment extends BaseFragment<AboutPresenter,IAboutView> imple
 
     @Override
     protected void init(View view, @Nullable Bundle savedInstanceState) {
-
+        mImei.setText("设备号："+AppMsgUtil.getIMEI(getActivity()));
     }
 
     @Override
@@ -50,10 +62,24 @@ public class AboutFragment extends BaseFragment<AboutPresenter,IAboutView> imple
 
     @Override
     public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
-        if (enter){
-            return AnimationUtils.loadAnimation(getActivity(),R.anim.activity_anim_in);
-        }else {
-            return super.onCreateAnimation(transit,enter,nextAnim);
+        if (enter) {
+            return AnimationUtils.loadAnimation(getActivity(), R.anim.activity_anim_in);
+        } else {
+            return super.onCreateAnimation(transit, enter, nextAnim);
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
